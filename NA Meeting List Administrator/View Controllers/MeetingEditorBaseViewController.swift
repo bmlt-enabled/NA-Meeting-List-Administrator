17aa602e4c8b61bbfa837d85d99aed36e673bb92
Chris M <webmaster@magnaws.com>
2017-02-10 08:38:02 -0500

    Setting up the basics of dynamic table cells in the meeting editor.

--- NA Meeting List Administrator/View Controllers/MeetingEditorBaseViewController.swift
+++ NA Meeting List Administrator/View Controllers/MeetingEditorBaseViewController.swift
@@ -26,15 +26,49 @@ import BMLTiOSLib
 // MARK: - Base Single Meeting Editor View Controller Class -
 /* ###################################################################################################################################### */
 /**
+ This class describes the basic functionality for a full meeting editor.
  */
 class MeetingEditorBaseViewController : EditorViewControllerBaseClass, UITableViewDataSource {
+    /** This is a list of the indexes for our prototypes. */
+    enum PrototypeSectionIndexes: Int {
+        /** The Meeting Name Editable Section */
+        case MeetingNameSection = 0
+    }
+    
+    /** We use this as a common prefix for our reuse IDs, and the index as the suffix. */
+    let reuseIDBase = "editor-row-"
+    
+    /** This is the meeting object for this instance. */
+    var meetingObject: BMLTiOSLibEditableMeetingNode! = nil
+    
+    /** This is a list of all the cells (editor sections). */
+    var editorSections: [MeetingEditorViewCell] = []
+    
+    @IBOutlet var tableView: UITableView!
+    
+    /** The following are sections in our table. Each is described by a prototype. */
+    
+    /** These are the three "dynamics" in the editor. */
+    /** If true, then we allow the meeting to be deleted. */
     @IBInspectable var showDelete: Bool!
+    /** If true, then we allow the meeting to be saved as a duplicate. */
     @IBInspectable var showDuplicate: Bool!
+    /** If true, then we allow meeting history to be show. */
     @IBInspectable var showHistory: Bool!
     
-    @IBOutlet var containerTableView: UITableView!
-
-    var meetingObject: BMLTiOSLibEditableMeetingNode! = nil
+    /* ################################################################## */
+    // MARK: Overridden Base Class Methods
+    /* ################################################################## */
+    /**
+     Called as the view is about to appear.
+     
+     - parameter animated: True, if the appearance is animated.
+     */
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.editorSections = []
+        self.tableView.reloadData()
+    }
     
     /* ################################################################## */
     // MARK: UITableViewDataSource Methods
@@ -46,7 +80,12 @@ class MeetingEditorBaseViewController : EditorViewControllerBaseClass, UITableVi
      - returns the number of rows to display.
      */
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        // Got this tip from here: http://natecook.com/blog/2014/10/loopy-random-enum-ideas/
+        var max: Int = 0
+        
+        while let _ = PrototypeSectionIndexes(rawValue: max) { max += 1 }
+        
+        return max
     }
     
     /* ################################################################## */
@@ -59,6 +98,53 @@ class MeetingEditorBaseViewController : EditorViewControllerBaseClass, UITableVi
      - returns a nice, shiny cell (or sets the state of a reused one).
      */
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reuseID = self.reuseIDBase + String(indexPath.row)
+        
+        if let returnableCell = tableView.dequeueReusableCell(withIdentifier: reuseID) as? MeetingEditorViewCell {
+            returnableCell.meetingObject = self.meetingObject
+            self.editorSections.append(returnableCell)
+            return returnableCell
+        }
+        
         return UITableViewCell()
+    }
+}
+
+/* ###################################################################################################################################### */
+// MARK: - Meeting Name Editor Table Cell Class -
+/* ###################################################################################################################################### */
+/**
+ This is the base table view class for the various cell prototypes.
+ */
+class MeetingEditorViewCell: UITableViewCell {
+    private var _meetingObject: BMLTiOSLibEditableMeetingNode! = nil
+    
+    var meetingObject: BMLTiOSLibEditableMeetingNode! {
+        get { return self._meetingObject }
+        set {
+            self._meetingObject = newValue
+            self.meetingObjectUpdated()
+        }
+    }
+    
+    func meetingObjectUpdated() {
+        
+    }
+}
+
+/* ###################################################################################################################################### */
+// MARK: - Meeting Name Editor Table Cell Class -
+/* ###################################################################################################################################### */
+/**
+ This is the table view class for the name editor prototype.
+ */
+class MeetingNameEditorTableViewCell: MeetingEditorViewCell {
+    /** This is the meeting name section. */
+    @IBOutlet weak var meetingNameLabel: UILabel!
+    @IBOutlet weak var meetingNameTextField: UITextField!
+    
+    override func meetingObjectUpdated() {
+        self.meetingNameLabel.text = NSLocalizedString(self.meetingNameLabel.text!, comment: "")
+        self.meetingNameTextField.placeholder = NSLocalizedString(self.meetingNameTextField.placeholder!, comment: "")
     }
 }
